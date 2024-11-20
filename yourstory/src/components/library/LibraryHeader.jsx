9faf3e623c003c816a0a505f65f7e570eb413aa2
kangjuen <00112233pq@gmail.com>
2024-11-20 22:16:31 +0900

    Fix: 날아간 거 복구+navbar props

--- yourstory/src/components/library/LibraryHeader.jsx
+++ yourstory/src/components/library/LibraryHeader.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 const LibraryHeader = () => {
   return (
     <HeaderContainer>
+      <Title>이타적 자서전 도서 목록</Title>
     </HeaderContainer>
   );
 };
@@ -12,5 +13,9 @@ const HeaderContainer = styled.div`
   margin-bottom: ${({ theme }) => theme.spacing.padding.lg};
 `;
 
+const Title = styled.h1`
+  font-size: ${({ theme }) => theme.typography.fontSize.xl};
+  font-weight: ${({ theme }) => theme.typography.fontWeight.bold};
+`;
 
 export default LibraryHeader;
--- yourstory/src/pages/Library.js
+++ yourstory/src/pages/Library.js
@@ -8,16 +8,14 @@ import { media } from '../styles/theme';
 const Library = () => {
   return (
     <div>
-      <NavBar />
+      <NavBar pagename="library" />
       <PageContainer>
         <LibraryHeader />
-        <Title>이타적 자서전 도서 목록</Title>
         <LibraryContent />
       </PageContainer>
     </div>
   );
 };
-
 
 const PageContainer = styled.div`
   padding: 81px 5%;
@@ -34,11 +32,5 @@ const PageContainer = styled.div`
     margin-left: 16.5625rem;
   }
 `;
-
-const Title = styled.h1`
-  font-size: ${({ theme }) => theme.typography.fontSize.xl};
-  font-weight: ${({ theme }) => theme.typography.fontWeight.bold};
-`;
-
 
 export default Library;
