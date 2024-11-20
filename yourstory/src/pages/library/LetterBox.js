5f15f0878841a2a0542c09bc5d1f141ab52085de
ynzung <syj91105@sookmyung.ac.kr>
2024-11-20 23:31:13 +0900

    Feat: 편지 컴포넌트 초기설정

--- yourstory/src/pages/library/LetterBox.js
+++ yourstory/src/pages/library/LetterBox.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import NavBar from "../../components/common/NavBar";
 import theme from "../../styles/theme";
 import LibraryHeader from "../../components/library/LibraryHeader";
+import LetterList from "../../components/library/LetterList";
 
 const LetterBox = () => {
   return (
@@ -10,6 +11,11 @@ const LetterBox = () => {
       <NavBar pagename={"library"} />
       <PageContainer>
         <LibraryHeader />
+        <HeaderTitle>
+          우편함
+          <HeaderLine />
+        </HeaderTitle>
+        <LetterList />
       </PageContainer>
     </div>
   );
@@ -21,4 +27,20 @@ const PageContainer = styled.div`
   padding: 81px 5%;
   margin-left: 16.5625rem;
   font-family: Inter;
+`;
+
+const HeaderTitle = styled.div`
+  font-weight: ${({ theme }) => theme.typography.fontWeight.bold};
+  font-size: 24px;
+  color: var(--green);
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-left: 1rem;
+`;
+
+const HeaderLine = styled.div`
+  border-bottom: 2px solid var(--green);
+  flex: 1;
+  height: 1px;
 `;
