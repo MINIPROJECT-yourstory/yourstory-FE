b36207dfead7512f01feae4f3ace1614d7eed879
ynzung <syj91105@sookmyung.ac.kr>
2024-11-26 18:51:38 +0900

    Feat: 필드 추가

--- yourstory/src/components/volunteer/VolunteerInfo.jsx
+++ yourstory/src/components/volunteer/VolunteerInfo.jsx
@@ -27,7 +27,11 @@ const VolunteerInfo = ({ workId }) => {
       setIsLoading(true);
       setError(null);
       const data = await volunteerApi.getVolunteerDetail(workId);
-      setVolunteerInfo(data);
+      setVolunteerInfo({
+        ...data,
+        manager: data.manager || "-",
+        content: data.content || "-",
+      });
     } catch (error) {
       console.error("봉사 상세 정보 조회 실패:", error);
       setError(
@@ -59,6 +63,10 @@ const VolunteerInfo = ({ workId }) => {
       value: volunteerInfo.time || "-",
       info: "-",
     },
+    {
+      value: volunteerInfo.manager || "-",
+      info: "-",
+    },
   ];
 
   return (
@@ -82,8 +90,14 @@ const VolunteerInfo = ({ workId }) => {
         </InfoTable>
 
         <ContentBox>
-          <BoldText>{volunteerInfo.centerTitle || "-"}</BoldText>
-          <Text>{volunteerInfo.centerContent || "-"}</Text>
+          <BoldText>
+            {volunteerInfo.centerTitle ||
+              "센터에서 홀몸 어르신의 이타적 자서전 작성을 함께할 자원봉사자를 모집합니다. "}
+          </BoldText>
+          <Text>
+            {volunteerInfo.content ||
+              "우리 센터에는 70대에서 80대의 어르신 다섯 분과 함께하고 있습니다. 울고 웃으며 여러 해를 함께해왔습니다. 센터에서의 활동을 넘어서 청년 여러분들과의 만남을 통해 힘을 얻으시는 할머님, 할아버님의 모습을 보며 올해 하반기 다시 한 번 청년 자원봉사자를 모집합니다. 처음에는 처음 뵙는 어르신과의 시간이 어색했지만, 점차 마음을 열며 여러 이야기들에 눈시울을 붉히던 이전 청년 분들이 기억납니다. 오늘 이 글과 함께 다시 한 번 어르신들의 이야기를 세상에 전할 여러분을 기다립니다. 숙멋사 팀장 드림문의사항은 아래 전화번호로 부탁드립니다. 전화번호: 0X-XXXX-XXXX"}
+          </Text>
         </ContentBox>
       </Container>
     </>
